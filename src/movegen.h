d914aa7e1c348f42cd54e08e4872d2ef9836a3b1
sarinaanneperez-dev <sarinaanneperez@gmail.com>
2026-07-08 17:33:26 +0800

    Refactor move generation functions and bit helpers

--- src/movegen.h
+++ src/movegen.h
@@ -8,24 +8,36 @@ class Board;
 
 void movegen_init();
 
-// Precomputed leaper attacks (initialized by movegen_init()).
 extern U64 KNIGHT_ATTACKS[64];
 extern U64 KING_ATTACKS[64];
-extern U64 PAWN_ATTACKS[2][64]; // by color, from square
+extern U64 PAWN_ATTACKS[2][64];
 
-// Classical ray-scan slider attacks (correct, no magics).
 U64 bishop_attacks(int sq, U64 occ);
 U64 rook_attacks(int sq, U64 occ);
 inline U64 queen_attacks(int sq, U64 occ) { return bishop_attacks(sq, occ) | rook_attacks(sq, occ); }
 
-// Bit helpers
-inline int  popcount(U64 x) { return __builtin_popcountll(x); }
-inline int  lsb(U64 x) { return __builtin_ctzll(x); }
-inline int  pop_lsb(U64& x) { int s = lsb(x); x &= x - 1; return s; }
+// Portable bit helpers
+#ifdef _MSC_VER
+    #include <intrin.h>
+    #pragma intrinsic(_BitScanForward64, _popcnt64)
+    inline int popcount(U64 x) { return static_cast<int>(_popcnt64(x)); }
+    inline int lsb(U64 x) {
+        unsigned long idx;
+        _BitScanForward64(&idx, x);
+        return static_cast<int>(idx);
+    }
+#else
+    inline int popcount(U64 x) { return __builtin_popcountll(x); }
+    inline int lsb(U64 x)     { return __builtin_ctzll(x); }
+#endif
 
-// Move generation. Both produce pseudo-legal moves; legality is checked in
-// Board::make_move by verifying own king is not attacked.
+inline int pop_lsb(U64& x) {
+    int s = lsb(x);
+    x &= x - 1;
+    return s;
+}
+
 void generate_all_moves(Board& b, MoveList& list);
-void generate_captures(Board& b, MoveList& list); // captures + promotions
+void generate_captures(Board& b, MoveList& list);
 
 } // namespace jaishi
